cffc2ed18a300463d88c26adc5a46c550f0daea7
Dimitrios Kastanis <dimitrios.kastanis@gmail.com>
2018-10-04 16:48:24 +0300

    word boundary fix

--- src/main/java/org/jimkast/exotic/parse/regex/dir_word_boundary.java
+++ src/main/java/org/jimkast/exotic/parse/regex/dir_word_boundary.java
@@ -6,6 +6,6 @@ import org.jimkast.exotic.binary.binary;
 public final class dir_word_boundary implements dir {
     @Override
     public int test(binary b, int pos, List<binary> groups) {
-        return pos == 0 || Character.isLetter(b.at(pos - 1)) ? 0 : -1;
+        return pos == 0 || Character.isLetter(b.at(pos - 1)) != Character.isLetter(b.at(1)) || b.length() == pos ? 0 : -1;
     }
 }
