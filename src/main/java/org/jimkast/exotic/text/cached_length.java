3bd995387a74aae78220ed2635ad8da2f3046b8e
Dimitrios Kastanis <dimitrios.kastanis@gmail.com>
2018-10-19 11:57:41 +0300

    removed duplicate IntSticky

--- src/main/java/org/jimkast/exotic/text/cached_length.java
+++ src/main/java/org/jimkast/exotic/text/cached_length.java
@@ -2,14 +2,14 @@ package org.jimkast.exotic.text;
 
 import org.jimkast.exotic.binary.binary;
 import org.jimkast.exotic.binary.blength;
-import org.jimkast.exotic.numberjdk.IntSticky;
+import org.jimkast.exotic.numberjdk.NumSticky;
 
 public final class cached_length implements binary {
     private final binary b;
     private final Number len;
 
     public cached_length(binary b) {
-        this(b, new IntSticky(new blength(b)));
+        this(b, new NumSticky(new blength(b)));
     }
 
     private cached_length(binary b, Number len) {
