1d34846954837f1af42dcc6033b1a0523686dc28
Dimitrios Kastanis <dimitrios.kastanis@gmail.com>
2018-10-26 15:32:54 +0300

    utf8 byte4

--- src/main/java/org/jimkast/ooj/source/ints/Utf8Decoder.java
+++ src/main/java/org/jimkast/ooj/source/ints/Utf8Decoder.java
@@ -13,7 +13,7 @@ public final class Utf8Decoder implements IntSource {
 
     public Utf8Decoder(IntSource origin) {
         this(origin, new Choose<>(
-            new Byte1(),
+            new Byte4(origin),
             new Case<>(
                 new ChkLt(0b10000000),
                 new Byte1()
@@ -21,6 +21,10 @@ public final class Utf8Decoder implements IntSource {
             new Case<>(
                 new ChkLt(0b11100000),
                 new Byte2(origin)
+            ),
+            new Case<>(
+                new ChkLt(0b11100000),
+                new Byte3(origin)
             )
         ));
     }
@@ -70,19 +74,16 @@ public final class Utf8Decoder implements IntSource {
         }
     }
 
-//    public static int byteArrayToInt(byte[] b) {
-//        return b[3] & 0xFF |
-//            (b[2] & 0xFF) << 8 |
-//            (b[1] & 0xFF) << 16 |
-//            (b[0] & 0xFF) << 24;
-//    }
-//
-//    public static byte[] intToByteArray(int a) {
-//        return new byte[]{
-//            (byte) ((a >> 24) & 0xFF),
-//            (byte) ((a >> 16) & 0xFF),
-//            (byte) ((a >> 8) & 0xFF),
-//            (byte) (a & 0xFF)
-//        };
-//    }
+    public static final class Byte4 implements IntSource2 {
+        private final IntSource s;
+
+        public Byte4(IntSource s) {
+            this.s = s;
+        }
+
+        @Override
+        public Cond feed(int b, IntTarget target) {
+            return s.feed(b2 -> s.feed(b3 -> s.feed(b4 -> target.accept(b + b2 + b3 + b4))));
+        }
+    }
 }
