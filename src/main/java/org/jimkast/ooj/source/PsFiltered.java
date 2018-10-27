f5ab56beeb5b4eb2f5d4363cb7a9c1040d4805c6
jimkast <jimkast.ath@gmail.com>
2018-10-27 14:29:55 +0300

    refactored ps filtered constructor

--- src/main/java/org/jimkast/ooj/source/PsFiltered.java
+++ src/main/java/org/jimkast/ooj/source/PsFiltered.java
@@ -1,21 +1,37 @@
 package org.jimkast.ooj.source;
 
 import org.jimkast.ooj.cond.Check;
+import org.jimkast.ooj.map.Mapping;
 
 public final class PsFiltered<T> extends PSource.Env<T> {
     public PsFiltered(Check<T> check, PSource<T> origin) {
-        this(new Check.Not<>(check), Target.Noop.instance(), origin);
-    }
-
-    private PsFiltered(Check<T> check, Target<T> noop, PSource<T> origin) {
         super(new PsStore<>(
-            new PsMapping<>(store ->
-                new PsWithFixedTarget<>(noop,
-                    new PsForEach<>(
-                        new PsWhile<>(check, store, origin)
-                    )
+            new PsMapping<>(
+                new FilteredMapping<>(
+                    new Check.Not<>(check),
+                    origin
                 )
             )
         ));
+    }
+
+
+    public static final class FilteredMapping<T> implements Mapping<Target<T>, PSource<T>> {
+        private final Check<T> check;
+        private final PSource<T> origin;
+
+        public FilteredMapping(Check<T> check, PSource<T> origin) {
+            this.check = check;
+            this.origin = origin;
+        }
+
+        @Override
+        public PSource<T> map(Target<T> store) {
+            return new PsWithFixedTarget<>(Target.Noop.instance(),
+                new PsForEach<>(
+                    new PsWhile<>(check, store, origin)
+                )
+            );
+        }
     }
 }
